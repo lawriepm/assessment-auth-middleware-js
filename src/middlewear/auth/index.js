854cb522c598d70317de4fc4af477ec50d6ec6d1
SSlpm <37372596+SSlpm@users.noreply.github.com>
2022-01-15 13:01:54 +0000

    Update middlewear to validate claims

--- src/middlewear/auth/index.js
+++ src/middlewear/auth/index.js
@@ -4,33 +4,68 @@ import jwkToPem from 'jwk-to-pem';
 
 
 export default class JwtAuthenticator {
-  async #getKeys() {
-    const { data: { keys } } = await axios.get('http://issuer.com/.well-known/jwks.json');
+  constructor() {
+    this.USER_POOL_ID = 'http://issuer.com'; // mock value for verifying claims
+    this.CLIENT_ID = 'audience'; // mock value for verifying claims
+  }
+
+  async #fetchKeys() {
+    const { data: { keys } } = await axios.get(`${this.USER_POOL_ID}/.well-known/jwks.json`);
     return keys;
   }
 
-  async #decodeToken(token) {
-    if (!token) return [false];
+  #verifyClaims(decodedToken) {
+    const settings = [{
+      key: 'exp',
+      isValid: (arg) => Math.round(Date.now() / 1000) < arg
+    }, {
+      key: 'iss',
+      isValid: (arg) => arg.endsWith(this.USER_POOL_ID)
+    }, {
+      key: 'aud',
+      isValid: (arg) => arg === this.CLIENT_ID,
+    }];
 
-    const keys = await this.#getKeys();
-    const pem = jwkToPem(keys[0]);
-
-    return jwt.verify(token, pem, { algorithms: ['RS256'] }, function(err, decodedToken) {
-      if (err) {
-        return [false];
-      };
-
-      return [true, decodedToken];
-    });
+    return settings.some((setting) => setting.isValid(decodedToken[setting.key]));
   }
-  
+
+  #decodeTokenHeader(token) {
+    const [headerEncoded] = token.split('.');
+    const buff = new Buffer(headerEncoded, 'base64');
+    const text = buff.toString('ascii');
+    return JSON.parse(text);
+  }
+
+  #verify(err, decodedToken) {
+    if (err) {
+      return [false];
+    };
+
+    const areClaimsValid = this.#verifyClaims(decodedToken);
+    if (!areClaimsValid) {
+      return [false];
+    }
+
+    return [true, decodedToken];
+  }
+
+  async #decodeTokenSignature(token) {
+    if (!token) return [false];
+    const keys = await this.#fetchKeys();
+    const { kid, alg } = this.#decodeTokenHeader(token);
+    const key = keys.find(({ kid: keyId }) => kid === keyId);
+    const pem = jwkToPem(key);
+    return jwt.verify(token, pem, { algorithms: [alg] }, this.#verify.bind(this));
+  }
+
   async authenticateToken(req, res, next) {
     const {
       headers: {
         authorizationinfo,
       }
     } = req;
-    const [isValid, decodedToken] = await this.#decodeToken(authorizationinfo);
+
+    const [isValid, decodedToken] = await this.#decodeTokenSignature(authorizationinfo);
     
     if (!isValid) {
       res.status(401).send();
